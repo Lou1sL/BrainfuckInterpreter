8a4858db39327fa2db5240502d2f306b7b9f0655
留白 <156890093@qq.com>
2016-07-14 00:07:44 +0800

    Revert "Revert "bfinter is the main proj,Brainfuck Interpreter""
    This reverts commit 225f75838db24619da7330f2a2aa30c0fff7f530.

---
+++ brainfuckInterpreter/brainfuckInterpreter/brainfuckInterpreter/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+// 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
+
+// 如果要为以前的 Windows 平台生成应用程序，请包括 WinSDKVer.h，并将
+// WIN32_WINNT 宏设置为要支持的平台，然后再包括 SDKDDKVer.h。
+#include <SDKDDKVer.h>
+
+
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
---
+++ brainfuckInterpreter/brainfuckInterpreter/brainfuckInterpreter/wind.cpp
@@ -0,0 +1,23 @@
+
+#include "stdafx.h"
+#include "wind.h"
+
+using namespace brainfuckInterpreter;
+
+[STAThread]
+
+int main(array<System::String^>^args)
+{
+	Application::EnableVisualStyles();
+	Application::Run(gcnew wind());
+
+
+	
+
+
+	return 0;
+
+}
+
+
+
